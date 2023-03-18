6dd35e539d1bf6f62e49d0c8ea07679f34bead87
Day RM <35691884+DayrisRM@users.noreply.github.com>
2023-03-18 12:37:32 +0100

    add one-point crossover (#5)

---
+++ GrammaticalEvolution/Services/OnePointCrossoverService.cs
@@ -0,0 +1,77 @@
+﻿using GrammaticalEvolution.Abstractions;
+using GrammaticalEvolution_Common.Models;
+
+namespace GrammaticalEvolution.Services
+{
+    public class OnePointCrossoverService : ICrossoverService
+    {
+        private RandomGeneratorNumbersService _randomGeneratorNumbersService { get; set; }
+
+        public OnePointCrossoverService()
+        {
+            _randomGeneratorNumbersService = new RandomGeneratorNumbersService();
+        }
+
+        public OnePointCrossoverService(RandomGeneratorNumbersService randomGeneratorNumbersService)
+        {
+            _randomGeneratorNumbersService = randomGeneratorNumbersService;
+        }
+
+        public List<Individual> Cross(List<Individual> parents)
+        {            
+            var firstParent = parents[0];
+            var secondParent = parents[1];            
+
+            return CreateChilds(firstParent, secondParent);
+        }
+
+        private List<Individual> CreateChilds(Individual firstParent, Individual secondParent)
+        {
+            var childs = new List<Individual>();            
+            var cutPoints = GetPointCross(firstParent, secondParent);
+            var pointCrossFistParent = cutPoints.Item1;
+            var pointCrossSecondParent = cutPoints.Item2;
+
+
+            var firstPartParent1 = GetSegmentFromList(firstParent.Genotype, 0, pointCrossFistParent);
+            var secondPartParent1 = GetSegmentFromList(firstParent.Genotype, pointCrossFistParent, firstParent.Genotype.Count);
+
+            var firstPartParent2 = GetSegmentFromList(secondParent.Genotype, 0, pointCrossSecondParent);
+            var secondPartParent2 = GetSegmentFromList(secondParent.Genotype, pointCrossSecondParent, secondParent.Genotype.Count);
+
+            var child1Genotype = new List<int>();
+            child1Genotype.AddRange(firstPartParent1);
+            child1Genotype.AddRange(secondPartParent2);
+
+            var child2Genotype = new List<int>();
+            child2Genotype.AddRange(firstPartParent2);
+            child2Genotype.AddRange(secondPartParent1);
+
+            childs.Add(new Individual() { Genotype = child1Genotype });
+            childs.Add(new Individual() { Genotype = child2Genotype });
+
+            return childs;
+
+        }
+
+        private Tuple<int, int> GetPointCross(Individual firstParent, Individual secondParent)
+        {
+            var pointCrossFistParent = _randomGeneratorNumbersService.GetInt(1, firstParent.Genotype.Count - 1);
+            var pointCrossSecondParent = pointCrossFistParent;
+            if (firstParent.Genotype.Count != secondParent.Genotype.Count)
+            {               
+                pointCrossSecondParent = _randomGeneratorNumbersService.GetInt(1, secondParent.Genotype.Count - 1);
+            }
+
+            return new Tuple<int, int> (pointCrossFistParent, pointCrossSecondParent);
+        }
+
+        private List<int> GetSegmentFromList(List<int> genotype, int firstCutPoint, int secondCutPoint)
+        {
+            return genotype
+                .Skip(firstCutPoint)
+                .Take((secondCutPoint - firstCutPoint))
+                .ToList();
+        }       
+    }
+}
---
+++ GrammaticalEvolution_UnitTests/OnePointCrossoverServiceUnitTests.cs
@@ -0,0 +1,108 @@
+﻿using GrammaticalEvolution.Services;
+using GrammaticalEvolution_Common.Models;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrammaticalEvolution_UnitTests
+{
+    public class OnePointCrossoverServiceUnitTests
+    {
+        
+        [TestCase("0,0,0,0,0,0", "1,1,1,1,1,1", "0,0,0,0,1,1", "1,1,1,1,0,0", 4)]
+        [TestCase("0,0,0,0,0,0", "1,1,1,1,1,1", "0,0,0,0,0,1", "1,1,1,1,1,0", 5)]
+        [TestCase("0,0,0,0,0,0", "1,1,1,1,1,1", "0,1,1,1,1,1", "1,0,0,0,0,0", 1)]
+        [TestCase("1,2,3,4,5,6", "1,1,1,1,1,1", "1,2,3,4,5,1", "1,1,1,1,1,6", 5)]
+        public void OnePointCrossoverService_GenotypeSameSize_ShouldCross(string genotype1St, string genotype2St, string expectedGenotype1St, string expectedGenotype2St, int cutPoint)
+        {           
+            var parents = new List<Individual>()
+            {
+                new Individual()
+                {
+                    Genotype = genotype1St.Split(',').Select(int.Parse).ToList()
+                },
+                new Individual()
+                {
+                    Genotype = genotype2St.Split(',').Select(int.Parse).ToList()
+                }
+            };
+
+            var mockService = new Mock<RandomGeneratorNumbersService>();
+            mockService
+                .Setup(m => m.GetInt(1, parents[0].Genotype.Count - 1))
+                .Returns(cutPoint);
+
+            var _onePointCrossoverService = new OnePointCrossoverService(mockService.Object);
+
+
+            var childs = _onePointCrossoverService.Cross(parents);
+            Assert.IsNotNull(childs);
+            Assert.That(2 == childs.Count, Is.True);
+
+            var expectedGenotype1 = expectedGenotype1St.Split(',').Select(int.Parse).ToList();
+            var expectedGenotype2 = expectedGenotype2St.Split(',').Select(int.Parse).ToList();
+
+            CheckChild(childs[0].Genotype, expectedGenotype1);
+            CheckChild(childs[1].Genotype, expectedGenotype2);
+        }
+
+        [TestCase("0,0,0,0,0,0", "1,1,1,1,1,1,1,1,1,1", "0,0,0,0,1,1,1,1,1", "1,1,1,1,1,0,0", 4, 5)]
+        [TestCase("1,2,3,4,5,6", "1,1,1,1,1,1,1,1,1,1", "1,2,3,4,5,1,1", "1,1,1,1,1,1,1,1,6", 5, 8)]
+        [TestCase("1,2,3,4,5,6,7,8,9,10", "1,1,1,1,1,1,1", "1,2,3,4,5,6,7,1,1,1,1,1", "1,1,8,9,10", 7, 2)]
+        public void OnePointCrossoverService_GenotypeDiferentSize_ShouldCross(string genotype1St, string genotype2St, string expectedGenotype1St, string expectedGenotype2St, int cutPointParent1, int cutPointParent2)
+        {
+            var parents = new List<Individual>()
+            {
+                new Individual()
+                {
+                    Genotype = genotype1St.Split(',').Select(int.Parse).ToList()
+                },
+                new Individual()
+                {
+                    Genotype = genotype2St.Split(',').Select(int.Parse).ToList()
+                }
+            };
+
+            var mockService = new Mock<RandomGeneratorNumbersService>();
+            mockService
+                .Setup(m => m.GetInt(1, parents[0].Genotype.Count - 1))
+                .Returns(cutPointParent1);
+
+            mockService
+                .Setup(m => m.GetInt(1, parents[1].Genotype.Count - 1))
+                .Returns(cutPointParent2);
+
+            var _onePointCrossoverService = new OnePointCrossoverService(mockService.Object);
+
+
+            var childs = _onePointCrossoverService.Cross(parents);
+            Assert.IsNotNull(childs);
+            Assert.That(2 == childs.Count, Is.True);
+
+            var expectedGenotype1 = expectedGenotype1St.Split(',').Select(int.Parse).ToList();
+            var expectedGenotype2 = expectedGenotype2St.Split(',').Select(int.Parse).ToList();
+
+            CheckChild(childs[0].Genotype, expectedGenotype1);
+            CheckChild(childs[1].Genotype, expectedGenotype2);
+        }
+
+
+        private void CheckChild(List<int> genotype, List<int> expectedGenotype)
+        {            
+            Assert.IsTrue(expectedGenotype.Count == genotype.Count);
+
+            for(var i = 0; i < expectedGenotype.Count; i++) 
+            {                
+                Assert.That(expectedGenotype[i], Is.EqualTo(genotype[i]));
+            }
+        }
+       
+
+
+
+    }
+}
