8ff8b685fec83977289fe69dee087c88fae145e1
Day RM <35691884+DayrisRM@users.noreply.github.com>
2023-03-19 12:41:52 +0100

    Add RandomResettingMutationService (#9)

---
+++ GrammaticalEvolution/Services/RandomResettingMutationService.cs
@@ -0,0 +1,57 @@
+﻿using GrammaticalEvolution.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrammaticalEvolution.Services
+{
+    public class RandomResettingMutationService : IMutationService<List<int>, List<int>>
+    {
+        private readonly IRandomGeneratorNumbersService _randomGeneratorNumbersService;
+        private double _mutationProbability { get; set; }
+        private Tuple<int, int> _mutationIndex { get; set; }
+
+        public RandomResettingMutationService(IRandomGeneratorNumbersService randomGeneratorNumbersService, 
+            double mutationProbability, Tuple<int, int> mutationIndex)
+        {
+            _randomGeneratorNumbersService = randomGeneratorNumbersService;
+            _mutationProbability = mutationProbability;
+            _mutationIndex = mutationIndex;
+        }
+        public List<int> Mutate(List<int> individuals)
+        {
+            var mutatedInd = new List<int>();
+
+            foreach (var individual in individuals) 
+            {
+                var p = _randomGeneratorNumbersService.GetDouble();
+                if (p < _mutationProbability)
+                {
+                    var newInd = GetIndividualValue(individual);
+                    mutatedInd.Add(newInd);
+
+                }
+                else 
+                {
+                  mutatedInd.Add(individual);
+                }
+            }
+
+            return mutatedInd;
+        }
+
+        private int GetIndividualValue(int individual)
+        {
+            var newInd = _randomGeneratorNumbersService.GetInt(_mutationIndex.Item1, _mutationIndex.Item2);
+            if (newInd == individual)
+            {
+                newInd = GetIndividualValue(individual);
+            }
+
+            return newInd;
+        }
+    }
+}
+
---
+++ GrammaticalEvolution_UnitTests/RandomResettingMutationServiceUnitTests.cs
@@ -0,0 +1,48 @@
+﻿using GrammaticalEvolution.Services;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrammaticalEvolution_UnitTests
+{
+    public class RandomResettingMutationServiceUnitTests
+    {
+        [TestCase("0,1,2,3,4,5", 0.2, 0.9, "255,255,255,255,255,255")]
+        [TestCase("0,1,2,3,4,5", 0.95, 0.9, "0,1,2,3,4,5")]
+        public void RandomResettingMutationService_xx(string genotype1St, double probMutationCodon, double pm, string expectedGenotype1St) 
+        {
+            var genotype = genotype1St.Split(',').Select(int.Parse).ToList();
+            var expectedGenotype = expectedGenotype1St.Split(',').Select(int.Parse).ToList();
+            var mutationIndex = new Tuple<int, int>(0, 255);
+
+            var mockService = new Mock<RandomGeneratorNumbersService>();
+            mockService
+                .Setup(m => m.GetDouble())
+                .Returns(probMutationCodon);
+
+            mockService
+                .Setup(m => m.GetInt(mutationIndex.Item1, mutationIndex.Item2))
+                .Returns(255);            
+
+            var _onePointCrossoverService = new RandomResettingMutationService(mockService.Object, pm, mutationIndex);
+            var mutatedElement = _onePointCrossoverService.Mutate(genotype);
+            Assert.NotNull(mutatedElement);
+            CheckChild(mutatedElement, expectedGenotype);
+        }
+
+        private void CheckChild(List<int> genotype, List<int> expectedGenotype)
+        {
+            Assert.IsTrue(expectedGenotype.Count == genotype.Count);
+
+            for (var i = 0; i < expectedGenotype.Count; i++)
+            {
+                Assert.That(expectedGenotype[i], Is.EqualTo(genotype[i]));
+            }
+        }
+
+    }
+}
